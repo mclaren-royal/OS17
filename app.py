5994c578ffbe432b0df3926b5cfe287999526f1b
mclaren-royal <mclarenololade2@gmail.com>
2026-02-10 07:10:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-u
+from flask import Flask, request, jsonify
+from model import brain
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "MCLAREN OS 1: Online. Created by MCLARENXCODE Team."
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['input'] # Expecting something like [0, 1]
+    prediction = brain.think(np.array(data))
+    return jsonify({
+        "status": "Success",
+        "output": prediction.tolist(),
+        "engine": "MCLAREN OS 1"
+    })
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
