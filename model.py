4f889f3c1bf32aa7270fef43685977dafad98b89
mclaren-royal <mclarenololade2@gmail.com>
2026-02-10 07:10:23 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,20 @@
 
+import numpy as np
+
+class McLarenEngine:
+    def __init__(self):
+        # 2 inputs, 3 hidden neurons, 1 output
+        self.weights1 = np.random.rand(2, 3) 
+        self.weights2 = np.random.rand(3, 1)
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def think(self, inputs):
+        # The Forward Pass
+        layer1 = self.sigmoid(np.dot(inputs, self.weights1))
+        output = self.sigmoid(np.dot(layer1, self.weights2))
+        return output
+
+# Initialize the system
+brain = McLarenEngine()
